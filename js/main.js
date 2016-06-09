00cad9f1a01783e0130b56ed0ae96a534f6bf5c3
SandraBollo <sandra.bollo.b@gmail.com>
2016-06-09 18:33:45 -0500

    avance

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//     $('#fondo1').click(function(){
+//         $('#fondo1').slideToggle( { direction: "left" }, 1000);
+//     });
+// });
+
+
+$(document).ready(function(){
+ $(".latters1").addClass("animated tada");
+});
+$(document).ready(function(){
+ $(".latters").addClass("animated tada");
+});
+
+$(document).ready(function(){
+ $(".fls").addClass("animated fadelnLeft");
+});
+
+
